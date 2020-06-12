2bc42c28eae816949d75cf49121d0e5fa67b1109
Vinay <vinaykumarvk.8866@gmail.com>
2020-06-12 18:52:14 +0530

    Application is perfectly able to move a rectangle on game screen by events

--- snake.py
+++ snake.py
@@ -2,7 +2,13 @@ import pygame,sys, os
 from pygame.locals import *
 
 red=(255,0,0)
+catx=10
+caty=10
+screen=0
 
+def myquit():
+	pygame.quit()
+	sys.exit(0)
 
 def check_inputs(events):
 	global catx,caty,screen
@@ -14,14 +20,21 @@ def check_inputs(events):
 			if event.key == K_ESCAPE:
 				quit()
 			elif event.key == K_UP:
+				caty-=5
 				print('UP')
 			elif event.key == K_DOWN:
+				caty+=5
 				print('DOWN')
 			elif event.key == K_LEFT:
+				catx-=5
 				print('LEFT')
 			elif event.key == K_RIGHT:
+				catx+=5
 				print('RIGHT')
-
+	screen=pygame.display.get_surface()
+	screen.fill((0,0,0))
+	pygame.draw.rect(screen,(255,255,255),(catx,caty,50,50))
+	pygame.display.update()
 
 
 

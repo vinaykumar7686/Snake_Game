e20366d1140ea15e0f8f64c9dcd7afe29d66b70a
Vinay <vinaykumarvk.8866@gmail.com>
2020-06-12 18:38:26 +0530

    Application is perfectly able to handle Events

--- snake.py
+++ snake.py
@@ -3,23 +3,39 @@ from pygame.locals import *
 
 red=(255,0,0)
 
-#pygame.init()
 
-window=pygame.display.set_mode((1024,720))
-pygame.display.set_caption("Snake")
+def check_inputs(events):
+	global catx,caty,screen
 
-screen=pygame.display.get_surface()
-screen.fill(red)
+	for event in events:
+		if event.type == QUIT:
+			quit()
+		elif event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				quit()
+			elif event.key == K_UP:
+				print('UP')
+			elif event.key == K_DOWN:
+				print('DOWN')
+			elif event.key == K_LEFT:
+				print('LEFT')
+			elif event.key == K_RIGHT:
+				print('RIGHT')
 
-pygame.display.flip()
-
-while True:
-    for event in pygame.event.get():
-        print(event)
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        pygame.display.update()
-    pass
 
 
+
+def main():
+	pygame.init()
+	window=pygame.display.set_mode((1024,720))
+	pygame.display.set_caption("Snake")
+
+	screen=pygame.display.get_surface()
+	screen.fill(red)
+
+	pygame.display.flip()
+
+	while True:
+	    check_inputs(pygame.event.get())
+
+main()
